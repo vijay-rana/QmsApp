d10079bcf945d723ddaf46a0ede947cb8569e906
vedangsharma <kbs@kbss-Mac-mini.local>
2016-03-08 17:18:17 +0530

    add Constant
    add Constant

---
+++ QMS/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  QMS
+//
+//  Created by kbs on 3/8/16.
+//  Copyright © 2016 Techechelons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
---
+++ QMS/MainMenuViewController.m
@@ -0,0 +1,37 @@
+//
+//  MainMenuViewController.m
+//  QMS
+//
+//  Created by kbs on 3/8/16.
+//  Copyright © 2016 Techechelons. All rights reserved.
+//
+
+#import "MainMenuViewController.h"
+
+@interface MainMenuViewController ()
+
+@end
+
+@implementation MainMenuViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
