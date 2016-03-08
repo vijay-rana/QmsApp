93423d5c7d2084e67a13ff1263e561637d1674a5
amitkbs <amit.kamal@kindlebit.com>
2016-03-08 16:15:49 +0530

    QMS creaeted project
    QMS creaeted project

---
+++ Constants.h
@@ -0,0 +1,16 @@
+//
+//  Constants.h
+//  QMS
+//
+//  Created by KBS on 04/03/16.
+//  Copyright © 2016 Techechelons. All rights reserved.
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+
+#define SITE_URL                            @"http://qmswatch.com/"
+
+
+#endif /* Constants_h */
